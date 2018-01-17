0a90cdcd0b9d4362f1cb5ccad06aed4370fc5d85
Joseph Paul Cohen <joseph@josephpcohen.com>
2018-01-17 22:04:59 +0000

    added run script

---
+++ run.sh
@@ -0,0 +1,11 @@
+python3 -u mlbot.py  | tee -a log
+sleep 3600
+python3 -u mlbot.py  | tee -a log
+sleep 10000
+python3 -u mlbot.py  | tee -a log
+sleep 10000
+python3 -u mlbot.py  | tee -a log
+sleep 10000
+python3 -u mlbot.py  | tee -a log
+sleep 10000
+python3 -u mlbot.py  | tee -a log
